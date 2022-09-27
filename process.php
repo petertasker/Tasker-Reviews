e58b69945b01682cf3d05edef5aaa7c6214783e2
Peter Tasker <petertasker05@gmail.com>
2022-09-27 10:23:12 +0100

    Created a CSS, HTML, and PHP Page. The indel has a form with display:table styling. The form resulsts go to the PHP page with

---
+++ process.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Peters Guitar Logger!</title>
+  <meta charset="UTF-8" />
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+
+<?php
+$make = $_POST["make"];
+$brandName = $_POST["brandName"];
+$type = $_POST["type"];
+$wood = $_POST["wood"];
+$yearProduced = $_POST["yearProduced"];
+
+echo $make;
+echo $brandName;
+echo $type;
+echo $wood;
+echo $yearProduced;
+
+
+
+?>
+    
+</body>
+</html>
