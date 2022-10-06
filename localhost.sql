3a0c327d41a8431ed87cc6eb35f7b07768fa1d26
Peter <98236996+petertasker@users.noreply.github.com>
2022-10-06 09:39:53 +0100

    Update localhost.sql

--- localhost.sql
+++ localhost.sql
@@ -1,132 +1,39 @@
--- MariaDB dump 10.19  Distrib 10.4.24-MariaDB, for Win64 (AMD64)
---
--- Host: localhost    Database: guitardatabase
--- ------------------------------------------------------
--- Server version	10.4.24-MariaDB
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `brand`
---
-
-DROP TABLE IF EXISTS `brand`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `brand` (
-  `brandName` varchar(63) NOT NULL,
-  `dateEstablished` date NOT NULL,
-  PRIMARY KEY (`brandName`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `brand`
---
-
-LOCK TABLES `brand` WRITE;
-/*!40000 ALTER TABLE `brand` DISABLE KEYS */;
-/*!40000 ALTER TABLE `brand` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `guitar`
---
-
-DROP TABLE IF EXISTS `guitar`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `guitar` (
-  `guitarID` int(11) NOT NULL,
-  `make` varchar(50) NOT NULL,
-  `brandName` varchar(63) NOT NULL,
-  `cost` int(11) NOT NULL,
-  `pickups` varchar(255) NOT NULL,
-  `woodtype` varchar(63) NOT NULL,
-  `yearmade` int(11) NOT NULL,
-  `signature` tinyint(1) NOT NULL,
-  `signatureArtist` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`guitarID`),
-  KEY `brandName` (`brandName`),
-  CONSTRAINT `guitar_ibfk_1` FOREIGN KEY (`brandName`) REFERENCES `brand` (`brandName`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `guitar`
---
-
-LOCK TABLES `guitar` WRITE;
-/*!40000 ALTER TABLE `guitar` DISABLE KEYS */;
-/*!40000 ALTER TABLE `guitar` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `ownedguitars`
---
-
-DROP TABLE IF EXISTS `ownedguitars`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `ownedguitars` (
-  `userID` int(11) NOT NULL,
-  `guitarID` int(11) NOT NULL,
-  PRIMARY KEY (`userID`,`guitarID`),
-  KEY `guitarID` (`guitarID`),
-  CONSTRAINT `ownedguitars_ibfk_1` FOREIGN KEY (`guitarID`) REFERENCES `guitar` (`guitarID`),
-  CONSTRAINT `ownedguitars_ibfk_2` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `ownedguitars`
---
-
-LOCK TABLES `ownedguitars` WRITE;
-/*!40000 ALTER TABLE `ownedguitars` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ownedguitars` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users` (
-  `userID` int(11) NOT NULL,
-  `username` varchar(63) NOT NULL,
-  `password` varchar(63) NOT NULL,
-  PRIMARY KEY (`userID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-09-29 17:29:13
+CREATE TABLE Brand
+(
+  BrandName VARCHAR(100) NOT NULL,
+  YearEst INT NOT NULL,
+  PRIMARY KEY (brandName)
+);
+ 
+CREATE TABLE Users
+(
+  UserID INT NOT NULL,
+  Username VARCHAR(30) NOT NULL,
+  Password VARCHAR(100) NOT NULL,
+  PRIMARY KEY (UserID)
+);
+ 
+CREATE TABLE Guitar
+(
+  GuitarID INT NOT NULL,
+  Make VARCHAR(100) NOT NULL,
+  BrandName VARCHAR(100) NOT NULL,
+  Cost NUMERIC 13,2 NOT NULL,
+  YearMade INT NOT NULL,
+  Signature BOOL NOT NULL,
+  SignatureArtist VARCHAR(100),
+  brandName INT NOT NULL,
+  PRIMARY KEY (GuitarID),
+  FOREIGN KEY (BrandName) REFERENCES Brand(BrandName)
+);
+ 
+CREATE TABLE LoggedGuitars
+(
+  LoggedGuitarID INT NOT NULL,
+  UserID INT NOT NULL,
+  GuitarID INT NOT NULL,
+  PRIMARY KEY (LoggedGuitarID),
+  FOREIGN KEY (GuitarID) REFERENCES Guitar(GuitarID),
+  FOREIGN KEY (UserID) REFERENCES Users(UserID)
+);
