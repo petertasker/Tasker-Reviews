f8dcf890c24769d2957358070bfd4ccfdcd99b88
Peter <98236996+petertasker@users.noreply.github.com>
2022-10-11 09:17:05 +0100

    Update localhost.sql

--- localhost.sql
+++ localhost.sql
@@ -1,37 +1,36 @@
 
-CREATE TABLE Brand 
-( 
-  BrandName VARCHAR(100) NOT NULL, 
-  TimesLogged INT NOT NULL, 
-  PRIMARY KEY (brandName) 
-); 
-
-CREATE TABLE Users 
-( 
-  Username VARCHAR(30) NOT NULL, 
-  Password VARCHAR(100) NOT NULL, 
-  PRIMARY KEY (Username) 
-); 
-
-CREATE TABLE Guitar 
+CREATE TABLE Brand
 (
-  GuitarID INT NOT NULL, 
-  Make VARCHAR(100) NOT NULL, 
-  BrandName VARCHAR(100) NOT NULL, 
-  Cost NUMERIC 13,2 NOT NULL, 
-  YearMade INT NOT NULL, 
-  Signature BOOL NOT NULL, 
-  SignatureArtist VARCHAR(100), 
-  PRIMARY KEY (GuitarID), 
-  FOREIGN KEY (BrandName) REFERENCES Brand(BrandName) 
-); 
+    BrandName VARCHAR(100) NOT NULL,
+    TimesLogged INT NOT NULL,
+    PRIMARY KEY (brandName)
+);
 
-CREATE TABLE LoggedGuitars 
-( 
-  LoggedGuitarID INT NOT NULL, 
-  Username VARCHAR(30) NOT NULL, 
-  GuitarID INT NOT NULL, 
-  PRIMARY KEY (LoggedGuitarID), 
-  FOREIGN KEY (GuitarID) REFERENCES Guitar(GuitarID), 
-  FOREIGN KEY (Username) REFERENCES Users(Username) 
-); 
+CREATE TABLE Users
+(
+    Username VARCHAR(30) NOT NULL,
+    UserPassword VARCHAR(100) NOT NULL,
+    PRIMARY KEY (Username)
+);
+CREATE TABLE Guitar
+(
+    GuitarID INT NOT NULL,
+    Make VARCHAR(100) NOT NULL,
+    BrandName VARCHAR(100) NOT NULL,
+    Cost NUMERIC 13,2 NOT NULL,
+    YearMade INT NOT NULL,
+    Signed BOOL NOT NULL,
+    SignatureArtist VARCHAR(100),
+    PRIMARY KEY (GuitarID),
+    FOREIGN KEY (BrandName) REFERENCES Brand(BrandName)
+);  
+
+CREATE TABLE LoggedGuitar
+(
+    LoggedGuitarID INT NOT NULL,
+    Username VARCHAR(30) NOT NULL,
+    GuitarID INT NOT NULL,
+    PRIMARY KEY (LoggedGuitarID),
+    FOREIGN KEY (GuitarID) REFERENCES Guitar(GuitarID),
+    FOREIGN KEY (Username) REFERENCES Users(Username)
+);
