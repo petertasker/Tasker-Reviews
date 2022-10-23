22a3c5a9572aeff7767d0e1cbe626e8b18c50144
petertasker <petertasker05@gmail.com>
2022-10-23 16:13:21 +0100

    TODO: develop this page

---
+++ accessoryreview.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Accessory Review</title>   
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <p>Accessory Review</p>
+    </body>
+</html>
---
+++ guitarreview.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Guitar Review</title>   
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <section class="item-review">
+            <form>
+                <label for="make">Make of guitar*</label>
+                <input name="make" required>
+                
+                <label for="brand">Brand name*</label>
+                <input name="brand" required>
+            </form>
+        </section>
+        <section class="review-review">
+
+        </section>
+    </body>
+</html>
