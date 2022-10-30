7d7ea45d713e720a6acb187d60214f1a379dca68
petertasker <petertasker05@gmail.com>
2022-10-30 18:43:04 +0000

    Changed to facilitate prepared statements

--- config.php
+++ config.php
@@ -1,14 +1,18 @@
 <?php
-// this script is ran at runtime
+// This script is ran at runtime
 define("db_server", "localhost");
 define("db_username", "root");
 define("db_password", "");
 define("db_name", "guitar");
 
-// connect
-$conn = mysqli_connect(db_server, db_username, db_password, db_name);
 
-// check connection
+// This code automatically highlights errors
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+// Connection
+$conn = new mysqli(db_server, db_username, db_password, db_name);
+
+// Check connection
 if($conn === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
