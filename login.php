ce1b111c2078a52fc6c065411acfcca4f30f1acb
Peter <98236996+petertasker@users.noreply.github.com>
2023-01-27 22:16:55 +0000

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 require_once "config.php";
-if ((isset($_SESSION["username"])) || (isset($_SESSION["email"]))) {
+if (isset($_SESSION["username"])) {
     header("Location: index.php");
 }
 ?>
